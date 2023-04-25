a7fade7ef7234643835d16ace5849bdac355c7ae
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-04-25 10:43:49 -0400

    framed out styles and components

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+import RestaurantList from './components/RestaurantList';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RestaurantList />
     </View>
   );
 }
@@ -13,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/RestaurantList.jsx
@@ -0,0 +1,36 @@
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
+
+export default function RestaurantList() {
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}> Restaurant List </Text>
+      <ScrollView style={styles.list}></ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    fontWeight: 700,
+    marginVertical: 8,
+    color: '#151B54',
+  },
+
+  list: {
+    width: '100%',
+    borderColor: 'red',
+    borderWidth: 2,
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    marginTop: 32,
+    width: '90%',
+    marginBottom: 32,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+})
